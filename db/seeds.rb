bbdaaa3ea9866840330c87c31010d8c122e19ac9
yonathan marlow <yonathanmarlow@hotmail.com>
2020-06-23 13:09:09 +1000

    Updated pages. Still need players + standings)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,27 @@
 User.destroy_all
 u1 = User.create :email => 'yoni@yoni.com', :password => 'burger' #:admin => true
 u2 = User.create :email => 'craigsy@ga.co', :password => 'chicken'
+u2 = User.create :email => 'ivan@ivan.com', :password => 'ivan'
+u2 = User.create :email => 'james@james.com', :password => 'james'
+u2 = User.create :email => 'kale@kale.com', :password => 'kale'
 puts "#{ User.count } users"
 
 Player.destroy_all
 p1 = Player.create :price => 7,:name => 'Marcus Rashford', :position => 'Striker', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
 p2 = Player.create :price => 8,:name => 'Bruno Fernandez', :position => 'Midfielder', :image =>  'https://s.yimg.com/uu/api/res/1.2/etL9cKw4xPNftgypkbbEYQ--~B/aD0xMDgwO3c9MTkyMDtzbT0xO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/uk.goal.com/fafd2877cfee5b51abd54d16a9dca1df'
-p3 = Player.create :price => 6,:name => 'Paul Pogba', :position => 'Midfielder', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p4 = Player.create :price => 5,:name => 'David De Gea', :position => 'Goalkeeper', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p5 = Player.create :price => 5,:name => 'Harry Maguire', :position => 'Defender', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p6 = Player.create :price => 3,:name => 'Eric Bailly', :position => 'Defender', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p7 = Player.create :price => 5,:name => 'Victor Lindelof', :position => 'Defender', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p8 = Player.create :price => 1,:name => 'Luke Shaw', :position => 'Defender', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p9 = Player.create :price => 2,:name => 'Fred', :position => 'Midfielder', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p10 = Player.create :price => 6,:name => 'Anthony Martial', :position => 'Striker', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p11 = Player.create :price => 3,:name => 'Mason Greenwood', :position => 'Striker', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p12 = Player.create :price => 3,:name => 'Phil Jones', :position => 'Defender', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p13 = Player.create :price => 9,:name => 'Juan Mata', :position => 'Midfielder', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p14 = Player.create :price => 4,:name => 'Jesse Lingard', :position => 'Midfielder', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
-p15 = Player.create :price => 5,:name => 'Andreas Pereira', :position => 'Midfielder', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
+p3 = Player.create :price => 6,:name => 'Paul Pogba', :position => 'Midfielder', :image =>  'https://www.telegraph.co.uk/content/dam/football/2019/12/17/TELEMMGLPICT000218907322_trans%2B%2BhasgUMiR-rxiRxu9qBoVLWQtiI2hkxiZmOHW6aD3Ia4.jpeg'
+p4 = Player.create :price => 5,:name => 'David De Gea', :position => 'Goalkeeper', :image =>  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.skysports.com%2Ffootball%2Fnews%2F11095%2F11962647%2Fmanchester-uniteds-david-de-gea-and-peter-schmeichel-among-keepers-in-awe-of-kid-practising-in-isolation&psig=AOvVaw3zO2KdaI3RTIFBlMxeBeYj&ust=1592966209818000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjf6NrzluoCFQAAAAAdAAAAABAI'
+p5 = Player.create :price => 5,:name => 'Harry Maguire', :position => 'Defender', :image =>  'https://i2-prod.manchestereveningnews.co.uk/incoming/article18227548.ece/ALTERNATES/s615/1_GettyImages-1206106266.jpg'
+p6 = Player.create :price => 3,:name => 'Eric Bailly', :position => 'Defender', :image =>  'https://specials-images.forbesimg.com/imageserve/1201558632/960x0.jpg?cropX1=408&cropX2=2894&cropY1=0&cropY2=1990'
+p7 = Player.create :price => 5,:name => 'Victor Lindelof', :position => 'Defender', :image =>  'https://d3vlf99qeg6bpx.cloudfront.net/content/uploads/2020/03/26112651/Victor.Lindelof.jpg'
+p8 = Player.create :price => 1,:name => 'Luke Shaw', :position => 'Defender', :image =>  'https://images.daznservices.com/di/library/GOAL/a8/3/luke-shaw-manchester-united_fe2rlnbvhl2l13rb40rryc98n.jpg?t=2127942864&quality=60&w=1200&h=800'
+p9 = Player.create :price => 2,:name => 'Fred', :position => 'Midfielder', :image =>  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuNTGpYnEFSPdCifBPr-YFhPZN-2t4IT0ogxyG3fGNF4Hu8-Ja&usqp=CAU'
+p10 = Player.create :price => 6,:name => 'Anthony Martial', :position => 'Striker', :image =>  'https://i0.wp.com/sportytell.com/wp-content/uploads/2020/03/Anthony-Martial-Biography-Facts-Childhood-Net-Worth-Life-1.jpg?fit=912%2C516&ssl=1'
+p11 = Player.create :price => 3,:name => 'Mason Greenwood', :position => 'Striker', :image =>  'https://images.daznservices.com/di/library/GOAL/30/9a/mason-greenwood-manchester-united_iz8uk7h0nu8w1e4urnzldtiem.jpg?t=445418223&quality=100'
+p12 = Player.create :price => 3,:name => 'Phil Jones', :position => 'Defender', :image =>  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVzH-L1noff_-2azulSyNIWwpmjHXkq9DLQBsol4Y4uPwZqsm4&usqp=CAU'
+p13 = Player.create :price => 9,:name => 'Juan Mata', :position => 'Midfielder', :image =>  'https://i2-prod.manchestereveningnews.co.uk/incoming/article17222081.ece/ALTERNATES/s615/0_GettyImages-1180763564.jpg'
+p14 = Player.create :price => 4,:name => 'Jesse Lingard', :position => 'Midfielder', :image =>  'https://cdn.mos.cms.futurecdn.net/yfBR8qBLfTPtkniSC3PpXE-320-80.jpg'
+p15 = Player.create :price => 5,:name => 'Andreas Pereira', :position => 'Midfielder', :image =>  'https://s.hs-data.com/bilder/spieler/gross/252185.jpg'
 
 p16 = Player.create :price => 5,:name => 'Adrian', :position => 'Goalkeeper', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
 p17 = Player.create :price => 5,:name => 'Alisson Becker', :position => 'Goalkeeper', :image =>  'https://s.yimg.com/ny/api/res/1.2/OM4VUNDofo3xZ3vYjihw6A--~A/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAw/https://media.zenfs.com/en/uk.goal.com/edd3386640afaa7073aa73ac08ca6906'
@@ -72,9 +75,9 @@ puts "#{ Player.count } players available"
 
 Team.destroy_all
 t1 = Team.create :name => 'Manchester United', :established => '1881', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
-t2 = Team.create :name => 'Liverpool', :established => '1980', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
-t3 = Team.create :name => 'Manchester City', :established => '1562', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
-t4 = Team.create :name => 'Leicester', :established => '1111', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
+t2 = Team.create :name => 'Liverpool', :established => '1980', :country => 'England', :image => 'https://cdn.shopify.com/s/files/1/1888/7339/files/Liverpool_09_grande.jpg?v=1498035807'
+t3 = Team.create :name => 'Manchester City', :established => '1562', :country => 'England', :image => 'https://i.pinimg.com/originals/66/90/6f/66906f43adefb3bc553f7601fa7c1ed9.jpg'
+t4 = Team.create :name => 'Leicester', :established => '1111', :country => 'England', :image => 'https://cdn.dribbble.com/users/165349/screenshots/2692881/leicester_city_logo_update.png'
 # t5 = Team.create :name => 'Manchester City', :established => '1888', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
 # t6 = Team.create :name => 'Arsenal', :established => '2002', :country => 'England', :image => 'https://i.pinimg.com/originals/c0/85/dd/c085ddfcca1e66bd151ffb82a0a0d8a0.jpg'
 
@@ -82,15 +85,15 @@ puts "#{ Team.count } teams"
 
 Nationality.destroy_all
 n0 = Nationality.create :country => 'Unknown'
-n1 = Nationality.create :country => 'England', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n2 = Nationality.create :country => 'Spain', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n3 = Nationality.create :country => 'Netherlands', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n4 = Nationality.create :country => 'Brazil', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n5 = Nationality.create :country => 'Norway', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n6 = Nationality.create :country => 'Portugal', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n7 = Nationality.create :country => 'Ivory Coast', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n8 = Nationality.create :country => 'Sweden', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
-n9 = Nationality.create :country => 'Belgium', :image => 'https://abcsports.com.au/bmz_cache/d/d6e6319aa36cab636d8ecbabbd5f56d7.image.500x500.jpg'
+n1 = Nationality.create :country => 'England', :image => 'https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/1200px-Flag_of_England.svg.png'
+n2 = Nationality.create :country => 'Spain', :image => 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png'
+n3 = Nationality.create :country => 'Netherlands', :image => 'https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg'
+n4 = Nationality.create :country => 'Brazil', :image => 'https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg'
+n5 = Nationality.create :country => 'Norway', :image => 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg'
+n6 = Nationality.create :country => 'Portugal', :image => 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg'
+n7 = Nationality.create :country => 'Ivory Coast', :image => 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg'
+n8 = Nationality.create :country => 'Sweden', :image => 'https://upload.wikimedia.org/wikipedia/commons/6/65/Flag_of_Belgium.svg'
+n9 = Nationality.create :country => 'Belgium', :image => 'https://upload.wikimedia.org/wikipedia/en/4/4c/Flag_of_Sweden.svg'
 
 puts "#{ Nationality.count } nationalities"
 
